70d65002abb2125eb5349ecc80875b1a3096f39d
sundaramoorthigsm <sundaramoorthi1995>
2018-05-29 15:45:51 +0530

    Stringconcatenation

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class DataTypeExample {
+public static void main(String args[])
+{
+String firstname="sundhar";
+String secondname="balaji";
+System.out.println(firstname+secondname);
+}
+}
