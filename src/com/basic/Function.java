b802451fe0f0fe1bd3ff52786da15f7255528d59
sundaramoorthigsm <sundaramoorthi1995>
2018-05-30 12:34:39 +0530

    equality

---
+++ src/com/basic/Function.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class Function {
+	
+	public static void main(String args[])
+	{
+		int i = 5;
+		
+		if(i>=5)
+			
+			System.out.println("n is more than 5");
+		else if(i<5)
+			
+			System.out.println("n is less than 5");
+	}
+
+}
