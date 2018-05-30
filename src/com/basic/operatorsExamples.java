f13198607b9d89c2a3cd3469d1ff6c43dd6ef9b6
sundaramoorthigsm <sundaramoorthi1995>
2018-05-30 10:35:51 +0530

    While Loop

---
+++ src/com/basic/operatorsExamples.java
@@ -0,0 +1,26 @@
+package com.basic;
+
+public class operatorsExamples {
+
+	public static void main(String args[])
+	{
+		int count=40;
+		count++;
+		
+		count--;
+		int negcount=-30;
+		boolean isFalse = true;
+		
+		System.out.println(!isFalse);
+		
+		System.out.println(count>15);
+		System.out.println(count<20);
+		
+		System.out.println(count>=30);;
+		System.out.println(count<=20);
+		
+		System.out.println(count==40);
+		System.out.println(count!=30);
+		
+	}
+}
