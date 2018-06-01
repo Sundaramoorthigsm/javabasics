ee31f0e9ee82ea450be4e18fc64c8a517a18ce8a
sundaramoorthigsm <sundaramoorthi1995>
2018-06-01 15:10:49 +0530

    Array

---
+++ src/com/basic/Array.java
@@ -0,0 +1,19 @@
+package com.basic;
+
+public class Array {
+	public static void main(String args[]){  
+		  
+		int a[]=new int[5];//declaration and instantiation  
+		a[0]=1;//initialization  
+		a[1]=2;  
+		a[2]=3;  
+		a[3]=4;  
+		a[4]=5;  
+		  
+		//printing array  
+		for(int i=0;i<a.length;i++)//length is the property of array  
+		System.out.println(a[i]);  
+		  
+		}}  
+
+
