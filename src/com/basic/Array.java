b2e08c2e1290540cd69bfdb78082567e6e9f3ccb
sundaramoorthigsm <sundaramoorthi1995>
2018-06-01 15:33:11 +0530

    ArrayImplementation

--- src/com/basic/Array.java
+++ src/com/basic/Array.java
@@ -3,12 +3,19 @@ package com.basic;
 public class Array {
 	public static void main(String args[]){  
 		  
-		int a[]=new int[5];//declaration and instantiation  
-		a[0]=1;//initialization  
-		a[1]=2;  
+		int a[]=new int[10];//declaration and instantiation  
+		a[0]=5;//initialization  
+		a[1]=4;  
 		a[2]=3;  
-		a[3]=4;  
-		a[4]=5;  
+		a[3]=2;  
+		a[4]=1; 
+		a[5]=0;
+		a[6]=0;
+		a[7]=0;
+		
+		
+		
+				
 		  
 		//printing array  
 		for(int i=0;i<a.length;i++)//length is the property of array  
