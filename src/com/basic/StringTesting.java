cb038e40f183246c54f2e456ecc039ee9bde3060
sundaramoorthigsm <sundaramoorthi1995>
2018-06-12 10:42:12 +0530

    StringConcatenation

---
+++ src/com/basic/StringTesting.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+
+	class StringTesting {  
+		 public static void main(String args[]){  
+		   String s="Sundara"+"Moorthi";  
+		   System.out.println(s);//print function 
+		 }  
+		}  
+
